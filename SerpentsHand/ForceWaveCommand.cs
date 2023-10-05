1e599e957ea7aefc66783bc5dcbc8417439309b5
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-10-05 15:05:28 +0200

    aliases

--- SerpentsHand/ForceWaveCommand.cs
+++ SerpentsHand/ForceWaveCommand.cs
@@ -1,31 +1,32 @@
 ﻿using CommandSystem;
-using Exiled.Permissions.Extensions;
-using Exiled.API.Features;
-using Exiled.API.Enums;
-using PlayerRoles;
 using System;
 
 namespace SerpentsHand
 {
-    [CommandHandler(typeof(RemoteAdminCommandHandler))]
-    internal class ForceWaveCommand : ICommand
-    {
-        public string Command { get; set; } = "forceshwave";
-        public string Description { get; set; } = "Forces SH on next spawn wave!";
-        public string[] Aliases { get; set; } = new string[] { };
-        public bool Execute(ArraySegment<string> args, ICommandSender sender, out string response)
-        {
-            if (!sender.CheckPermission(PlayerPermissions.RoundEvents))
-            {
-                response = "You do not have permission to use this command!";
-                return false;
-            }
+	[CommandHandler(typeof(RemoteAdminCommandHandler))]
+	internal class ForceWaveCommand : ICommand
+	{
+		public string Command { get; set; } = "forceshwave";
+		public string Description { get; set; } = "Forces SH on next spawn wave!";
+		public string[] Aliases { get; set; } = new string[]
+		{
+			"forcesh",
+			"shwave"
+		};
 
-            Plugin.Instance.IsForced = true;
-            Plugin.Instance.IsSpawnable = true;
+		public bool Execute(ArraySegment<string> args, ICommandSender sender, out string response)
+		{
+			if (!sender.CheckPermission(PlayerPermissions.RoundEvents))
+			{
+				response = "You do not have permission to use this command!";
+				return false;
+			}
 
-            response = "Next Chaos wave will be SH!";
-            return true;
-        }
-    }
+			Plugin.Instance.IsForced = true;
+			Plugin.Instance.IsSpawnable = true;
+
+			response = "Next Chaos wave will be SH!";
+			return true;
+		}
+	}
 }
