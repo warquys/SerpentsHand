fa0a57c98cb37029d87cddb754dab62d0512f516
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-10-05 15:17:20 +0200

    a

--- SerpentsHand/EventHandlers.cs
+++ SerpentsHand/EventHandlers.cs
@@ -12,95 +12,95 @@ using System.Linq;
 
 namespace SerpentsHand
 {
-     internal sealed class EventHandlers
-     {
-          private Plugin plugin;
-          public EventHandlers(Plugin plugin) => this.plugin = plugin;
+	internal sealed class EventHandlers
+	{
+		private Plugin plugin;
+		public EventHandlers(Plugin plugin) => this.plugin = plugin;
 
-          private int Respawns = 0;
-          private int SHRespawns = 0;
-          private CoroutineHandle calcuationCoroutine;
+		private int Respawns = 0;
+		private int SHRespawns = 0;
+		private CoroutineHandle calcuationCoroutine;
 
-          public void OnRoundStarted()
-          {
-               Plugin.Instance.IsSpawnable = false;
-               Respawns = 0;
-               SHRespawns = 0;
-               Plugin.Instance.IsForced = false;
+		public void OnRoundStarted()
+		{
+			Plugin.Instance.IsSpawnable = false;
+			Respawns = 0;
+			SHRespawns = 0;
+			Plugin.Instance.IsForced = false;
 
-               if (calcuationCoroutine.IsRunning)
-                    Timing.KillCoroutines(calcuationCoroutine);
+			if (calcuationCoroutine.IsRunning)
+				Timing.KillCoroutines(calcuationCoroutine);
 
-               calcuationCoroutine = Timing.RunCoroutine(spawnCalculation());
-          }
+			calcuationCoroutine = Timing.RunCoroutine(spawnCalculation());
+		}
 
-          private IEnumerator<float> spawnCalculation()
-          {
-               while (true)
-               {
-                    yield return Timing.WaitForSeconds(1f);
+		private IEnumerator<float> spawnCalculation()
+		{
+			while (true)
+			{
+				yield return Timing.WaitForSeconds(1f);
 
-                    if (Round.IsEnded)
-                         break;
+				if (Round.IsEnded)
+					break;
 
-                    if (Math.Round(Respawn.TimeUntilSpawnWave.TotalSeconds, 0) != Plugin.Instance.Config.SpawnWaveCalculation)
-                         continue;
+				if (Math.Round(Respawn.TimeUntilSpawnWave.TotalSeconds, 0) != Plugin.Instance.Config.SpawnWaveCalculation)
+					continue;
 
-                    if (Respawn.NextKnownTeam == SpawnableTeamType.ChaosInsurgency)
-                        Plugin.Instance.IsSpawnable = Loader.Random.Next(100) <= Plugin.Instance.Config.SerpentsHand.SpawnChance &&
-                        Respawns >= Plugin.Instance.Config.SerpentsHand.RespawnDelay &&
-                        SHRespawns < Plugin.Instance.Config.SerpentsHand.MaxSpawns || Plugin.Instance.IsForced;
-            }
-          }
+				if (Respawn.NextKnownTeam == SpawnableTeamType.ChaosInsurgency)
+					Plugin.Instance.IsSpawnable = Loader.Random.Next(100) <= Plugin.Instance.Config.SerpentsHand.SpawnChance &&
+					Respawns >= Plugin.Instance.Config.SerpentsHand.RespawnDelay &&
+					SHRespawns < Plugin.Instance.Config.SerpentsHand.MaxSpawns || Plugin.Instance.IsForced;
+			}
+		}
 
-          public void OnRespawningTeam(RespawningTeamEventArgs ev)
-          {
-               if (Plugin.Instance.IsSpawnable || Plugin.Instance.IsForced)
-               {
-                    bool scpAlive = Player.List.Count(x => x.Role.Team == Team.SCPs) != 0;
-                    if (!scpAlive && !Plugin.Instance.Config.SerpentsHand.CanSpawnWithoutScps)
-                         return;
+		public void OnRespawningTeam(RespawningTeamEventArgs ev)
+		{
+			if (Plugin.Instance.IsSpawnable || Plugin.Instance.IsForced)
+			{
+				bool scpAlive = Player.List.Count(x => x.Role.Team == Team.SCPs) != 0;
+				if (!scpAlive && !Plugin.Instance.Config.SerpentsHand.CanSpawnWithoutScps)
+					return;
 
-                    List<Player> players = new();
-                    if (ev.Players.Count > Plugin.Instance.Config.SerpentsHand.MaxSquad)
-                         players = ev.Players.GetRange(0, Plugin.Instance.Config.SerpentsHand.MaxSquad);
-                    else
-                         players = ev.Players.GetRange(0, ev.Players.Count);
+				List<Player> players = new();
+				if (ev.Players.Count > Plugin.Instance.Config.SerpentsHand.MaxSquad)
+					players = ev.Players.GetRange(0, Plugin.Instance.Config.SerpentsHand.MaxSquad);
+				else
+					players = ev.Players.GetRange(0, ev.Players.Count);
 
-                    foreach (Player player in players)
-                    {
-                         if (player is null)
-                              continue;
-                         Plugin.Instance.Config.SerpentsHand.AddRole(player);
-                    }
-                    SHRespawns++;
-                    if (!string.IsNullOrEmpty(Plugin.Instance.Config.SerpentsHand.EntryAnnoucement))
-                         Cassie.Message(Plugin.Instance.Config.SerpentsHand.EntryAnnoucement, isSubtitles: Plugin.Instance.Config.SerpentsHand.Subtitles);
+				foreach (Player player in players)
+				{
+					if (player is null)
+						continue;
+					Plugin.Instance.Config.SerpentsHand.AddRole(player);
+				}
+				SHRespawns++;
+				if (!string.IsNullOrEmpty(Plugin.Instance.Config.SerpentsHand.EntryAnnoucement))
+					Cassie.Message(Plugin.Instance.Config.SerpentsHand.EntryAnnoucement, isSubtitles: Plugin.Instance.Config.SerpentsHand.Subtitles);
 
-                    if (Plugin.Instance.Config.SerpentsHand.EntryBroadcast.Duration > 0 || !string.IsNullOrEmpty(Plugin.Instance.Config.SerpentsHand.EntryBroadcast.Content))
-                         foreach (Player player in Player.List.Where(x => x.Role.Team == Team.SCPs))
-                              player.Broadcast(Plugin.Instance.Config.SerpentsHand.EntryBroadcast);
+				if (Plugin.Instance.Config.SerpentsHand.EntryBroadcast.Duration > 0 || !string.IsNullOrEmpty(Plugin.Instance.Config.SerpentsHand.EntryBroadcast.Content))
+					foreach (Player player in Player.List.Where(x => x.Role.Team == Team.SCPs))
+						player.Broadcast(Plugin.Instance.Config.SerpentsHand.EntryBroadcast);
 
-                    Plugin.Instance.IsSpawnable = false;
-                    Plugin.Instance.IsForced = false;
-                    ev.IsAllowed = false;
-                    ev.NextKnownTeam = SpawnableTeamType.None;
-               }
+				Plugin.Instance.IsSpawnable = false;
+				Plugin.Instance.IsForced = false;
+				ev.IsAllowed = false;
+				ev.NextKnownTeam = SpawnableTeamType.None;
+			}
 
-               Respawns++;
-          }
+			Respawns++;
+		}
 
-          public void OnEndingRound(EndingRoundEventArgs ev)
-          {
-               if (plugin.Config.SerpentsHand.TrackedPlayers.Count > 0)
-               {
-                    if (ev.ClassList.mtf_and_guards != 0 || ev.ClassList.scientists != 0) ev.IsRoundEnded = false;
-                    else if (ev.ClassList.class_ds != 0) ev.IsRoundEnded = false;
-                    else if (!plugin.Config.SerpentsHand.ScpsWinWithChaos && ev.ClassList.chaos_insurgents != 0) ev.IsRoundEnded = false;
-               }
-          }
+		public void OnEndingRound(EndingRoundEventArgs ev)
+		{
+			if (plugin.Config.SerpentsHand.TrackedPlayers.Count > 0)
+			{
+				if (ev.ClassList.mtf_and_guards != 0 || ev.ClassList.scientists != 0) ev.IsRoundEnded = false;
+				else if (ev.ClassList.class_ds != 0) ev.IsRoundEnded = false;
+				else if (!plugin.Config.SerpentsHand.ScpsWinWithChaos && ev.ClassList.chaos_insurgents != 0) ev.IsRoundEnded = false;
+			}
+		}
 
-          public void OnSpawned(SpawnedEventArgs ev) => 
-               RoundSummary.singleton.ChaosTargetCount = Plugin.Instance.Config.SerpentsHand.ScpsWinWithChaos ? 0 : Player.List.Count(p => p.IsCHI);
-     }
+		public void OnSpawned(SpawnedEventArgs ev) =>
+			RoundSummary.singleton.ChaosTargetCount = Plugin.Instance.Config.SerpentsHand.ScpsWinWithChaos ? 0 : Player.List.Count(p => p.IsCHI);
+	}
 }
--- SerpentsHand/Plugin.cs
+++ SerpentsHand/Plugin.cs
@@ -6,42 +6,42 @@ using Server = Exiled.Events.Handlers.Server;
 
 namespace SerpentsHand
 {
-    public class Plugin : Plugin<Config>
-    {
-        public override string Name => "Serpents Hand";
-        public override string Author => "yanox, Michal78900, Marco15453, Vicious Vikki & Misfiy";
-        public override Version RequiredExiledVersion => new(8, 2, 1);
-        public override Version Version => new(8, 2, 5);
+	public class Plugin : Plugin<Config>
+	{
+		public override string Name => "Serpents Hand";
+		public override string Author => "yanox, Michal78900, Marco15453, Vicious Vikki & Misfiy";
+		public override Version RequiredExiledVersion => new(8, 2, 1);
+		public override Version Version => new(8, 2, 5);
 
-        public static Plugin Instance;
+		public static Plugin Instance;
 
-        public bool IsSpawnable = false;
-        public bool IsForced = false;
+		public bool IsSpawnable = false;
+		public bool IsForced = false;
 
-        private EventHandlers eventHandlers;
+		private EventHandlers eventHandlers;
 
-        public override void OnEnabled()
-        {
-            Instance = this;
-            Config.SerpentsHand.Register();
-            eventHandlers = new EventHandlers(this);
+		public override void OnEnabled()
+		{
+			Instance = this;
+			Config.SerpentsHand.Register();
+			eventHandlers = new EventHandlers(this);
 
-            Server.RoundStarted += eventHandlers.OnRoundStarted;
-            Server.RespawningTeam += eventHandlers.OnRespawningTeam;
-            Server.EndingRound += eventHandlers.OnEndingRound;
-            base.OnEnabled();
-        }
+			Server.RoundStarted += eventHandlers.OnRoundStarted;
+			Server.RespawningTeam += eventHandlers.OnRespawningTeam;
+			Server.EndingRound += eventHandlers.OnEndingRound;
+			base.OnEnabled();
+		}
 
-        public override void OnDisabled()
-        {
-            CustomRole.UnregisterRoles();
-            Server.RoundStarted -= eventHandlers.OnRoundStarted;
-            Server.RespawningTeam -= eventHandlers.OnRespawningTeam;
-            Server.EndingRound -= eventHandlers.OnEndingRound;
+		public override void OnDisabled()
+		{
+			CustomRole.UnregisterRoles();
+			Server.RoundStarted -= eventHandlers.OnRoundStarted;
+			Server.RespawningTeam -= eventHandlers.OnRespawningTeam;
+			Server.EndingRound -= eventHandlers.OnEndingRound;
 
-            eventHandlers = null;
-            Instance = null;
-            base.OnDisabled();
-        }
-    }
+			eventHandlers = null;
+			Instance = null;
+			base.OnDisabled();
+		}
+	}
 }
