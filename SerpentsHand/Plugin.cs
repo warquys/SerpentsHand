6dfff193535593a90243bc35f9340822a5cfcbe8
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-11-25 22:12:45 +0100

    version bump

--- SerpentsHand/Plugin.cs
+++ SerpentsHand/Plugin.cs
@@ -12,7 +12,7 @@ namespace SerpentsHand
 		public override string Name => "Serpents Hand";
 		public override string Author => "yanox, Michal78900, Marco15453, Vicious Vikki & Misfiy";
 		public override Version RequiredExiledVersion => new(8, 3, 8);
-		public override Version Version => new(8, 3, 1);
+		public override Version Version => new(8, 3, 2);
 
 		public static Plugin Instance;
 
