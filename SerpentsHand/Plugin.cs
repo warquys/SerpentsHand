802526713cb9b1164e7e0eeae8ce7acb062b150b
Snivy Films <Snivy.Films@gmail.com>
2025-01-16 16:52:08 -0800

    9.4.0 Build

--- SerpentsHand/Plugin.cs
+++ SerpentsHand/Plugin.cs
@@ -13,7 +13,7 @@ namespace SerpentsHand
 	{
 		public override string Name => "Serpents Hand";
 		public override string Author => "yanox, Michal78900, Marco15453, Vicious Vikki & Misfiy";
-		public override Version RequiredExiledVersion => new(9, 3, 0);
+		public override Version RequiredExiledVersion => new(9, 4, 0);
 		public override Version Version => new(9, 0, 5);
 
 		public static Plugin Instance;
